81c10c4a814e4b33cacdbd1989cfa399708a22fb
Saket Nandanwar <70093885+whodissaket@users.noreply.github.com>
2022-02-12 15:31:53 +0530

    Added:Chat component

--- src/components/Forum.js
+++ src/components/Forum.js
@@ -1,25 +1,25 @@
-import React from "react"
+import React from "react";
 
-import { BrowserRouter as Router, Routes , Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { AuthProvider } from "../context/AuthContext"
+import { AuthProvider } from "../context/AuthContext";
 
-import Chats from "../components/Chats"
-import Login from "../components/Login"
+import Chats from "../components/Chats";
+import Login from "../components/Login";
 
 function Forum() {
   return (
-    <div style={{ fontFamily: 'Avenir' }}>
+    <div style={{ fontFamily: "Avenir" }}>
       <Router>
         <AuthProvider>
-          <Routes >
-            <Route path="/chats" element={<Chats/>} />
-            <Route path="/" element={<Login/>} />
+          <Routes>
+            <Route path="/chats" element={<Chats />} />
+            {/* <Route path="/" element={<Login/>} /> */}
           </Routes>
         </AuthProvider>
       </Router>
     </div>
-  )
+  );
 }
 
-export default Forum
+export default Forum;
