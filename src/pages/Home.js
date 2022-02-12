1e90b45fadecca25f249e12204eecc9e3236cbb4
Pranav <pranavyede@gmail.com>
2022-02-12 16:43:22 +0530

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,8 +29,10 @@ const Home = () => {
       <Navbar />
         <div className="page">.
         <div className="title">
-          <h1 id="a">Do you feel that what you do is never good enough?</h1>
-        <h2 id="a">Then you may be suffering from Imposter Syndrome</h2>
+          <h1 id="a"><u>Do you feel that what you do is never good enough?</u></h1>
+        <br/>
+        <h2 id="a">Then you may be suffering from Imposter Syndrome </h2>
+        <br/>
         <h2 id='b'>Imposter Syndrome affects your sense of self worth, happiness, progress and affects <br/> most of the people at some point in their lifetime. It may be at academic leevel or work level.<br/>Accepting your imposter syndrome will allow you to accept that you deserve <br/> your success and allow you to reach greater heights in you life.
         </h2>
         </div>
