1d0baf4889610d1294d543b344131d7d6bdb8583
Saket Nandanwar <70093885+whodissaket@users.noreply.github.com>
2022-02-12 13:17:28 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,165 +14,21 @@ import MenuItem from "@mui/material/MenuItem";
 import { Link } from "react-router-dom";
 
 import Navbar from "../components/Navbar";
-import '../css/Home.css'
+import "../css/Home.css";
 
 const pages = ["Home", "Login", "Sign Up"];
 // const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 const Home = () => {
   return (
-    
-    <div div className="navMenu" >
+    <div div className="navMenu">
       {/* Navbar Starts */}
       <Navbar />
-      <AppBar  position="static">
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
-            >
-              LOGO
-            </Typography>
 
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: "block", md: "none" },
-                }}
-              >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
-            >
-              LOGO
-            </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-              <Link
-                to="/"
-                style={{
-                  textDecoration: "none",
-                }}
-              >
-                <Button
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: "white", display: "block" }}
-                >
-                  Home
-                </Button>
-              </Link>
-              <Link
-                to="/login"
-                style={{
-                  textDecoration: "none",
-                }}
-              >
-                <Button
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: "white", display: "block" }}
-                >
-                  Login
-                </Button>
-              </Link>
-              <Link
-                to="/signup"
-                style={{
-                  textDecoration: "none",
-                }}
-              >
-                <Button
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: "white", display: "block" }}
-                >
-                  SignUp
-                </Button>
-              </Link>
-              <Link
-                to="/chats"
-                style={{
-                  textDecoration: "none",
-                }}>
-              <Button
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                Forums
-              </Button>
-              </Link>
-            </Box>
-           
-            {/* <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box> */}
-          </Toolbar>
-        </Container>
-      </AppBar>
-      {/* Navbar Ends here */}
-      
       <div className="page">
         <p>Home Page. COMING SOON</p>
-        </div>
-        </div>
+      </div>
+    </div>
   );
 };
 
