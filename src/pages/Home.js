a9d2650e936244f2f0d3280214879a803e31807f
Pranav <pranavyede@gmail.com>
2022-02-12 19:12:37 +0530

    card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,48 +16,85 @@ import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import "../css/Home.css";
 
+
+
+
 const pages = ["Home", "Login", "Sign Up"];
 // const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 const Home = () => {
   return (
-    <div div className="navMenu">
+    
+    <div >
+      <div div className="navMenu">
       {/* Navbar Starts */}
       <Navbar />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-
       <div className="page">
-        .
         <div className="title">
-          <h1 id="a">
-            <u>Do you feel that what you do is never good enough?</u>
-          </h1>
+          <h1 id="a"><u>Do you feel that what you do is never good enough?</u></h1>
           <br />
           <h2 id="a">Then you may be suffering from Imposter Syndrome </h2>
           <br />
-          <h2 id="b">
-            Imposter Syndrome affects your sense of self worth, happiness,
-            progress and affects <br /> most of the people at some point in
-            their lifetime. It may be at academic leevel or work level.
+          <h2 id='b'>Imposter Syndrome affects your sense of self worth, happiness, progress and affects <br /> most of the people at some point in their lifetime. It may be at academic leevel or work level.<br />Accepting your imposter syndrome will allow you to accept that you deserve <br /> your success and allow you to reach greater heights in you life.
             <br />
-            Accepting your imposter syndrome will allow you to accept that you
-            deserve <br /> your success and allow you to reach greater heights
-            in you life.
+            <br />
           </h2>
+          <hr />
         </div>
-      </div>
-    </div>
-  );
-};
+        </div>
+    </div><div>
 
-export default Home;
+          <div class="container">
+          <div class="row">
+          <div class="col-sm">
+          <div class="blog_post">
+          <div class="container_copy">
+            <h1>Name</h1>
+            <br/>
+            <h3>12 January 2019</h3>
+            <br/>
+            <h2>CSS Positioning</h2>
+            <h4>The position property specifies the type of positioning method used for an element (static, relative, absolute, fixed, or sticky).</h4>
+            </div>
+            </div>
+            </div>
+            <div class="col-sm">
+            <div class="blog_post">
+          <div class="container_copy">
+            <h1>Name</h1>
+            <br/>
+            <h3>12 January 2019</h3>
+            <br/>
+            <h2>CSS Positioning</h2>
+            <h4>The position property specifies the type of positioning method used for an element (static, relative, absolute, fixed, or sticky).</h4>
+            </div>
+            </div>
+            </div>
+            <div class="col-sm">
+            <div class="blog_post">
+          <div class="container_copy">
+            <h1>Name</h1>
+            <br/>
+            <h3>12 January 2019</h3>
+            <br/>
+            <h2>CSS Positioning</h2>
+            <h4>The position property specifies the type of positioning method used for an element (static, relative, absolute, fixed, or sticky).</h4>
+            </div>
+            </div>
+            </div>
+            </div>
+      </div>
+          </div>
+          <a class="btn_primary" href='#'>Read More</a>
+        </div>
+      
+
+    
+       
+        
+        
+        );
+        };
+
+        export default Home;
+        
