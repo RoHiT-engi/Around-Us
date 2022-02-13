07b1ef476cb80e536fb33d20de274a2ddf68e3e9
Pranav <pranavyede@gmail.com>
2022-02-13 19:08:01 +0530

    Revert "footer"
    This reverts commit b455faaf051844ce03ed56a35b727fd1d30e31ad.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ import BlogAdd from "./BlogAdd";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <div div className="navMenu">
         {/* Navbar Starts */}
         <Navbar />
@@ -120,85 +120,117 @@ const Home = () => {
           </div>
         </div>
 
+        <div>
+          <body class="div3">
+            <h1 class="FAQ_heading">FAQ's</h1>
+            <br />
+            <main>
+              <details>
+                <summary>What is Impostor Syndrome?</summary>
+                <div class="faq__content">
+                  <p class="faq__text">
+                    People suffering from impostor syndrome doubt their skills
+                    and accomplishments, live in fear of being exposed as not
+                    worthy of their position, and even downplay their success,
+                    attributing it all to luck or good fortune.
+                  </p>
+                </div>
+              </details>
+
+              <details>
+                <summary>What triggers imposter syndrome?</summary>
+                <div class="faq__content">
+                  <p>
+                    There are lots of situations that can trigger these
+                    feelings. Differing in some way from your peers – by race,
+                    gender, sexual orientation, age, religion – can fuel a sense
+                    of being a fraud. It's also exacerbated when being measured
+                    or evaluated. Whatever the trigger, these feelings can lead
+                    to damaging habits.
+                  </p>
+                </div>
+              </details>
+
+              <details>
+                <summary>How long does imposter syndrome last?</summary>
+                <div class="faq__content">
+                  <p>
+                    According to a 2020 review, 9%–82% of people experience
+                    impostor syndrome. The numbers may vary depending on who
+                    participates in a study. Many people experience symptoms for
+                    a limited time, such as in the first few weeks of a new job.
+                    For others, the experience can be lifelong.
+                  </p>
+                </div>
+              </details>
+
+              <details>
+                <summary>Is impostor syndrome a mental illness?</summary>
+                <div class="faq__content">
+                  <p>
+                    Imposter syndrome isn't a psychiatric diagnosis. This
+                    phenomenon is experienced equally by men and women and
+                    frequently coexists with depression and anxiety
+                  </p>
+                </div>
+              </details>
+
+              <details>
+                <summary>Is impostor syndrome a mental illness?</summary>
+                <div class="faq__content">
+                  <p>
+                    Imposter syndrome isn't a psychiatric diagnosis. This
+                    phenomenon is experienced equally by men and women and
+                    frequently coexists with depression and anxiety
+                  </p>
+                </div>
+              </details>
+
+              <details>
+                <summary>Can you self diagnose imposter syndrome?</summary>
+                <div class="faq__content">
+                  <p>
+                    Though the impostor phenomenon isn't an official diagnosis
+                    listed in the DSM, psychologists and others acknowledge that
+                    it is a very real and specific form of intellectual
+                    self-doubt. Impostor feelings are generally accompanied by
+                    anxiety and, often, depression.
+                  </p>
+                </div>
+              </details>
+
+              <details>
+                <summary>
+                  Can you be confident and have imposter syndrome?
+                </summary>
+                <div class="faq__content">
+                  <p>
+                    Everyone can experience a lack of confidence in their
+                    abilities from time to time. But people who experience
+                    impostor syndrome tend to be high achievers with diverse
+                    educational backgrounds and a fair amount of professional
+                    experience. Despite this, they may often experience feelings
+                    of inadequacy and self-doubt.
+                  </p>
+                </div>
+              </details>
+
+              <details>
+                <summary>Is imposter syndrome Linked to ADHD?</summary>
+                <div class="faq__content">
+                  <p>
+                    Attention Deficit Hyperactivity Disorder (ADHD) and imposter
+                    syndrome are a common pairing. You just want to stop beating
+                    yourself up, to stop feeling like a failure no matter what
+                    you achieve. You want to feel good enough on the inside
+                  </p>
+                </div>
+              </details>
+            </main>
+          </body>
+        </div>
       </div>
-      <footer class="footer">
-  <div class="footer__addr">
-    <h1 class="footer__logo">Around Us</h1>
-        
-    <h2>Contact</h2>
-    
-    <address>
-      Ramrao Adik Institute of Technology, Nerul, Navi Mumbai.<br/>
-          
-      <a class="footer__btn" href="mailto:pranavyede@gmail.com">Email Us</a>
-    </address>
-  </div>
-  
-  <ul class="footer__nav">
-    
-    
-    <li class="nav__item nav__item--extra">
-      <h2 class="nav__title">Technology</h2>
-      
-      <ul class="nav__ul nav__ul--extra">
-        <li>
-          <a href="#">Stacks</a>
-        </li>
-        
-        <li>
-          <a href="#">Software Design</a>
-        </li>
-        
-        <li>
-          <a href="#">Digital Signage</a>
-        </li>
-        <ul/>
-       
-        <li>
-          <a href="#">Facebook</a>
-        </li>
-        
-        <li>
-          <a href="#">Instagram</a>
-        </li>
-        
-        <li>
-          <a href="#">Twitter</a>
-        </li>
-      </ul>
-    </li>
-    
-    <li class="nav__item">
-      <h2 class="nav__title">Legal</h2>
-      
-      <ul class="nav__ul">
-        <li>
-          <a href="#">Privacy Policy</a>
-        </li>
-        
-        <li>
-          <a href="#">Terms of Use</a>
-        </li>
-        
-        <li>
-          <a href="#">Sitemap</a>
-        </li>
-      </ul>
-    </li>
-  </ul>
-  
-  <div class="legal">
-    <p>&copy; Around Us 2022. All rights reserved.</p>
-    
-    <div class="legal__links">
-      <span>Made by team Pied Piper <span class="heart">♥</span></span>
-    </div>
-  </div>
-</footer>
-      </div>
-      
-      
-    
+    </>
   );
 };
 
